840f83c89d09e79460ba4eda113a7b2c942dcfcc
fijirash <fijirash@yandex.ru>
2022-07-03 16:29:34 +0300

    correct

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,13 +1,11 @@
-import web.model.Car;
 import web.service.CarService;
 import web.service.CarServiceImpl;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        CarService cars = new CarServiceImpl();
-        System.out.println(cars.getById(3));
+        CarService car = new CarServiceImpl();
+        System.out.println(car.getAllCars());
+        
     }
 }
